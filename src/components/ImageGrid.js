a107f446234eb937126d3dd6a2434ccdb2764a2d
Alek <alek.jakimovski1@gmail.com>
2021-07-16 10:08:28 +0200

    update: hover zoom effect added, refactoring grid

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -28,12 +28,6 @@ const ImageGrid = () => {
     return (
         <div className="images-grid">
             <div className="grid-wrapper">
-
-                {/*<div className="image-container">
-                            <a href="#" className="image-link"><img src={item.image} alt={item.name} key={index} className="item-image" />
-                                <p className="image-name">{item.name}</p>
-                            </a>
-                        </div>*/}
                 {
                     data && data.length && data.map((item, index) => (
                         <div className="image-container" key={index}>
@@ -45,6 +39,7 @@ const ImageGrid = () => {
                     ))
 
                 }
+
             </div>
         </div>
     )
